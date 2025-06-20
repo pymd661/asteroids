35d6568fa30ff5374bbdb3bd14b0ae6596c3f668
myko_d <pythonmd661@gmail.com>
2025-06-20 14:47:22 -0400

    updated main.py for while loop and pygame.event.quit

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from constants import *
 game_on = True
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen.fill(color="black")
+    pygame.display.flip() 
     while game_on == True:
-        pygame.init()
-        screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
-        screen.fill(color="black")
-        pygame.display.flip() 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+
 
 if __name__ == "__main__":
     main()
