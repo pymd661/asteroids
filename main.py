cb3ced48527c5105408404a4f6baa13fe72610da
myko_d <pythonmd661@gmail.com>
2025-06-20 14:29:34 -0400

    updated main.py for pygame

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
 
-def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+game_on = True
 
+def main():
+    while game_on == True:
+        pygame.init()
+        screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
+        screen.fill(color="black")
+        pygame.display.flip() 
 
 if __name__ == "__main__":
     main()
