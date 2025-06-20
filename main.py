c0a4e2326f0d1505310a61ba31eeb80a39b0785f
myko_d <pythonmd661@gmail.com>
2025-06-20 15:50:43 -0400

    updated frames per second

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def main():
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
     screen.fill(color="black")
     pygame.display.flip() 
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while game_on == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        clock.tick(60)
 
 
 if __name__ == "__main__":
