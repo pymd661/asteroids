df26bea188b7573ba87fdb457b0ff31e515115f4
myko_d <pythonmd661@gmail.com>
2025-06-20 20:29:04 -0400

    updated player.py to fix the traingle, had to fix the order of operations

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 game_on = True
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
-    screen.fill(color="black")
-    pygame.display.flip() 
+    pygame.init() # initialize pygame
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT)) # create surface
+    player_1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2) # setting the location for player1
 
     clock = pygame.time.Clock()
     dt = 0
@@ -16,9 +16,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+        screen.fill(color="black")
+        player_1.draw(screen)
+        pygame.display.flip() 
         clock.tick(60)
 
+    
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,8 +1,22 @@
 from circleshape import CircleShape
+from constants import *
+import pygame
 
 class Player(CircleShape):
- 
+    rotation = 0
     def __init__(self, x, y):
-        super().__init__()
-        self.x = x
-        self.y = y
+        super().__init__(x,y,radius=PLAYER_RADIUS)
+    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen, color="white", line_width=2):
+        return pygame.draw.polygon(surface =screen,
+            color=color,
+            points= self.triangle(), 
+            width = line_width)
